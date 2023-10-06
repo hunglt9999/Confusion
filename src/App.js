6ddfae84511e4eaa9a9196ae679a24876475590e
hunglt <lost.in.cosmos99@gmail.com>
2023-10-06 13:46:13 +0700

    Components p1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import Menu from './components/MenuComponent';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand href="/">Xin chao</NavbarBrand>
           </div>
         </Navbar>
+
+        <Menu />
       </div>
     );
   }
